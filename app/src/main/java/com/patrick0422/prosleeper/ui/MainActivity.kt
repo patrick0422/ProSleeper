858cd8978ceb53d920a59ef5c73f9a743a409b22
양태웅 <didxodnd0422@gmail.com>
2022-04-02 19:26:18 +0900

    Connect BottomNavigationView with NavHostFragment

--- app/src/main/java/com/patrick0422/prosleeper/ui/MainActivity.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.patrick0422.prosleeper.ui
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseActivity
 import com.patrick0422.prosleeper.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment }
+    private val navController by lazy { navHostFragment.navController }
 
     override fun init() {
-
+        setUpBottomNavigationView()
     }
 
+    private fun setUpBottomNavigationView() {
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, AppBarConfiguration(setOf(
+            R.id.homeFragment,
+            R.id.statisticsFragment,
+            R.id.settingsFragment
+        )))
+    }
+
+    override fun onNavigateUp(): Boolean = navController.navigateUp() || super.onNavigateUp()
 }
