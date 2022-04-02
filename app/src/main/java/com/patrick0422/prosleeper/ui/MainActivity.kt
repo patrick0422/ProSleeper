f233872ba521bd4a6329efc6e3a9cc5d2d67fec8
양태웅 <didxodnd0422@gmail.com>
2022-04-02 17:12:53 +0900

    Make MainActivity extend BaseActivity

---
+++ app/src/main/java/com/patrick0422/prosleeper/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.patrick0422.prosleeper.ui
+
+import com.patrick0422.prosleeper.R
+import com.patrick0422.prosleeper.base.BaseActivity
+import com.patrick0422.prosleeper.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun init() {
+
+    }
+
+}
