d2d53c8d773cba674591c5f6dfa47ebc083974a0
양태웅 <didxodnd0422@gmail.com>
2022-04-03 00:13:04 +0900

    Show TimePickerDialog on click

--- app/src/main/java/com/patrick0422/prosleeper/ui/SettingsFragment.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/SettingsFragment.kt
@@ -1,13 +1,34 @@
 package com.patrick0422.prosleeper.ui
 
+import android.app.AlertDialog
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseFragment
+import com.patrick0422.prosleeper.databinding.DialogTimePickerBinding
 import com.patrick0422.prosleeper.databinding.FragmentSettingsBinding
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
 
-    override fun init() {
 
+    override fun init() = with(binding) {
+        textNotificationTime.setOnClickListener {
+            showTimePickerDialog()
+        }
     }
 
+    private fun showTimePickerDialog() {
+        val dialogTimePickerBinding: DialogTimePickerBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(requireContext()), R.layout.dialog_time_picker, null, false
+        )
+
+        val alertDialog = AlertDialog.Builder(requireContext(), R.style.TimePickerDialog)
+            .setView(dialogTimePickerBinding.root)
+            .create()
+
+        dialogTimePickerBinding.textApply.setOnClickListener { alertDialog.dismiss() }
+        dialogTimePickerBinding.textCancel.setOnClickListener { alertDialog.dismiss() }
+
+        alertDialog.show()
+    }
 }
