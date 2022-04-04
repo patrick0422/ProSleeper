efd43ba98b4add4a8b7553eacab753b483401540
양태웅 <didxodnd0422@gmail.com>
2022-04-04 12:22:44 +0900

    Show Notification

--- app/src/main/java/com/patrick0422/prosleeper/ui/HomeFragment.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/HomeFragment.kt
@@ -1,13 +1,39 @@
 package com.patrick0422.prosleeper.ui
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.content.ContextCompat.getSystemService
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseFragment
 import com.patrick0422.prosleeper.databinding.FragmentHomeBinding
+import com.patrick0422.prosleeper.util.Constants.CHANNEL_DESCRIPTION
+import com.patrick0422.prosleeper.util.Constants.CHANNEL_ID
+import com.patrick0422.prosleeper.util.Constants.CHANNEL_NAME
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val notificationManager: NotificationManager by lazy { getSystemService(requireContext(), NotificationManager::class.java) as NotificationManager }
 
-    override fun init() {
+    override fun init() = with(binding) {
+        createNotificationChannel()
 
+        floatingActionButton.setOnClickListener { makeNotification() }
     }
 
+
+    private fun makeNotification() {
+        val notification = Notification.Builder(requireContext(), CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_sunny)
+            .setContentTitle("일어나셨나요?")
+            .setContentText("기상 시각을 기록해주세요!")
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+    private fun createNotificationChannel() {
+        notificationManager.createNotificationChannel(
+            NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply { description = CHANNEL_DESCRIPTION }
+        )
+    }
 }
--- app/src/main/java/com/patrick0422/prosleeper/util/Constants.kt
+++ app/src/main/java/com/patrick0422/prosleeper/util/Constants.kt
@@ -1,5 +1,7 @@
 package com.patrick0422.prosleeper.util
 
 object Constants {
-
+    const val CHANNEL_ID = "com.patrick0422.prosleeper"
+    const val CHANNEL_NAME = "prosleeper_channel_name"
+    const val CHANNEL_DESCRIPTION = "prosleeper_channel_description"
 }
