d51294c7a12f2cd9e72a20d0986be640578033ab
양태웅 <didxodnd0422@gmail.com>
2022-04-05 08:03:35 +0900

    Test Room in HomeFragment

--- app/src/main/java/com/patrick0422/prosleeper/ui/HomeFragment.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/HomeFragment.kt
@@ -4,40 +4,71 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import androidx.core.content.ContextCompat.getSystemService
-import com.airbnb.lottie.LottieAnimationView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.asLiveData
+import com.google.android.material.snackbar.Snackbar
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseFragment
+import com.patrick0422.prosleeper.data.local.WakeUpTimeEntity
 import com.patrick0422.prosleeper.databinding.FragmentHomeBinding
 import com.patrick0422.prosleeper.util.Constants.CHANNEL_DESCRIPTION
 import com.patrick0422.prosleeper.util.Constants.CHANNEL_ID
 import com.patrick0422.prosleeper.util.Constants.CHANNEL_NAME
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDateTime
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val notificationManager: NotificationManager by lazy { getSystemService(requireContext(), NotificationManager::class.java) as NotificationManager }
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun init() = with(binding) {
         createNotificationChannel()
+
+
 
         buttonWakeUp.setOnClickListener { onWakeUp() }
         floatingActionButton.setOnClickListener { makeNotification() }
     }
 
     private fun onWakeUp()  {
-        setLottieSrc()
+        if (mainViewModel.isTodaysWakeUpTimeSaved) {
+            makeSnackbar(R.string.on_already_saved_message)
+        } else {
+            applyLottieStyle()
+            saveWakeUpTime()
+            makeSnackbar(R.string.on_wakeup_time_saved_message)
+        }
     }
 
-    private fun setLottieSrc() {
+    private fun saveWakeUpTime() {
+        val wakeUpTimeEntity = WakeUpTimeEntity(LocalDateTime.now())
+        mainViewModel.insertWakeUpTime(wakeUpTimeEntity)
+    }
+
+    private fun applyLottieStyle() {
         binding.buttonWakeUp.setAnimation(R.raw.check)
         binding.buttonWakeUp.loop(false)
         binding.buttonWakeUp.playAnimation()
     }
 
+    fun checkIsTodaysWakeUpTimeSaved() {
+        mainViewModel.getWakeUpTimes().asLiveData().observe(viewLifecycleOwner) {
+
+        }
+    }
+
+    private fun makeSnackbar(id: Int) {
+        Snackbar.make(binding.root, getString(id), Snackbar.LENGTH_LONG)
+            .setAction("옙") {}
+            .show()
+    }
 
     private fun makeNotification() {
         val notification = Notification.Builder(requireContext(), CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_sunny)
-            .setContentTitle("일어나셨나요?")
-            .setContentText("기상 시각을 기록해주세요!")
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_content))
             .build()
 
         notificationManager.notify(1, notification)
--- app/src/main/java/com/patrick0422/prosleeper/ui/MainActivity.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.patrick0422.prosleeper.ui
 
+import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -7,8 +8,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseActivity
 import com.patrick0422.prosleeper.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val mainViewModel: MainViewModel by viewModels()
     private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment }
     private val navController by lazy { navHostFragment.navController }
 
