a2438b3d5fd6f342b3bb4bffc461ec7d6abdbf63
양태웅 <didxodnd0422@gmail.com>
2022-04-06 21:16:39 +0900

    Test Dynamic LineChart

--- app/src/main/java/com/patrick0422/prosleeper/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/statistics/StatisticsFragment.kt
@@ -2,9 +2,7 @@ package com.patrick0422.prosleeper.ui.statistics
 
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.asLiveData
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseFragment
@@ -14,33 +12,51 @@ import com.patrick0422.prosleeper.ui.MainViewModel
 
 class StatisticsFragment : BaseFragment<FragmentStatisticsBinding>(R.layout.fragment_statistics) {
     private val mainViewModel: MainViewModel by activityViewModels()
-
+    private val entries = mutableListOf<Entry>()
 
     override fun init() = with(binding) {
         getWakeUpTimes()
+        buttonTest.setOnClickListener { addItem() }
+        chart.invalidate()
     }
 
     private fun getWakeUpTimes() {
         mainViewModel.getWakeUpTimes().asLiveData().observe(viewLifecycleOwner) { result ->
-            setChart(result)
+//            setChart(result)
+            testChart()
+        }
+    }
+
+    private fun addItem() {
+        entries.add(Entry(entries.size.toFloat(), (1..24).random().toFloat()))
+        binding.chart.apply {
+            data = LineData(LineDataSet(entries, "Test"))
+            invalidate()
+        }
+    }
+
+    private fun testChart() {
+        binding.chart.apply {
+            data = LineData(LineDataSet(entries, "Test"))
+            invalidate()
         }
     }
 
     private fun setChart(result: List<WakeUpTimeEntity>) {
         val entries = result.map {
-            BarEntry(it.id.toFloat(), it.wakeUpTime.minute.toFloat())
+            Entry(it.id.toFloat(), it.wakeUpTime.minute.toFloat())
         }.toMutableList()
 
-        val barDataSet = BarDataSet(entries, "Test")
+        val dataSet = LineDataSet(entries, "Test")
 
-        binding.barChart.apply {
+        binding.chart.apply {
             xAxis.valueFormatter = DateAxisValueFormat()
             xAxis.setDrawLabels(true)
             xAxis.axisMinimum = 0f
             xAxis.axisMaximum = 24f
             
 
-            data = BarData(barDataSet)
+            data = LineData(dataSet)
             invalidate()
         }
     }
