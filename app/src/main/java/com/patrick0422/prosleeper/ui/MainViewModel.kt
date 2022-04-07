82f24df8b54d610309845a49ee0f66d3a2373cda
양태웅 <didxodnd0422@gmail.com>
2022-04-07 16:02:22 +0900

    Link MainViewModel and SettingsFragment with DataStoreRepository

--- app/src/main/java/com/patrick0422/prosleeper/ui/MainViewModel.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.patrick0422.prosleeper.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.patrick0422.prosleeper.data.DataStoreRepository
 import com.patrick0422.prosleeper.data.Repository
 import com.patrick0422.prosleeper.data.local.WakeUpTimeEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,9 +13,24 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val repository: Repository
+    private val repository: Repository,
+    private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
     var isTodaysWakeUpTimeSaved = false
+
+    /** DataStore */
+    val isNotificationAllowed = dataStoreRepository.readIsNotificationAllowed.asLiveData()
+    val notificaitonTime = dataStoreRepository.readNotificationTime.asLiveData()
+
+    fun saveIsNotificationAllowed(isAllowed: Boolean) = viewModelScope.launch {
+        dataStoreRepository.saveIsNotificationAllowed(isAllowed)
+    }
+
+    fun saveNotificationTime(notificationTime: String) = viewModelScope.launch {
+        dataStoreRepository.saveNotificationTime(notificationTime)
+    }
+
+    /** Remote */
 
     fun getWakeUpTimes(): Flow<List<WakeUpTimeEntity>> = repository.getWakeUpTimes()
 
--- app/src/main/java/com/patrick0422/prosleeper/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/patrick0422/prosleeper/ui/settings/SettingsFragment.kt
@@ -3,17 +3,31 @@ package com.patrick0422.prosleeper.ui.settings
 import android.app.AlertDialog
 import android.view.LayoutInflater
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.patrick0422.prosleeper.R
 import com.patrick0422.prosleeper.base.BaseFragment
 import com.patrick0422.prosleeper.databinding.DialogTimePickerBinding
 import com.patrick0422.prosleeper.databinding.FragmentSettingsBinding
+import com.patrick0422.prosleeper.ui.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalTime
 
+@AndroidEntryPoint
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
-
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun init() = with(binding) {
         textNotificationTime.setOnClickListener {
             showTimePickerDialog()
+        }
+        mainViewModel.notificaitonTime.observe(viewLifecycleOwner) { notificationTime ->
+            textNotificationTime.text = notificationTime
+        }
+        switchAllowNotification.setOnCheckedChangeListener { _, isChecked ->
+            mainViewModel.saveIsNotificationAllowed(isChecked)
+        }
+        mainViewModel.isNotificationAllowed.observe(viewLifecycleOwner) { isAllowed ->
+            switchAllowNotification.isChecked = isAllowed
         }
     }
 
@@ -26,9 +40,20 @@ class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment
             .setView(dialogTimePickerBinding.root)
             .create()
 
-        dialogTimePickerBinding.textApply.setOnClickListener { alertDialog.dismiss() }
-        dialogTimePickerBinding.textCancel.setOnClickListener { alertDialog.dismiss() }
+        with(dialogTimePickerBinding) {
+            textApply.setOnClickListener {
+            saveNotificationTime(timePicker.hour, timePicker.minute)
+                alertDialog.dismiss()
+            }
+            textCancel.setOnClickListener { alertDialog.dismiss() }
+        }
 
         alertDialog.show()
+    }
+
+    private fun saveNotificationTime(hour: Int, minute: Int) {
+        val notificationTime = LocalTime.of(hour, minute).toString()
+        binding.textNotificationTime.text = notificationTime
+        mainViewModel.saveNotificationTime(notificationTime)
     }
 }
